a86f28324d6dcb970ef241085fd76037bb450702
AlekseyKar <alekseykar92@gmail.com>
2022-12-12 19:01:32 +0300

    feat/s-3: refactor marvelServce.js in hooks

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,12 +3,14 @@ import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
+import ComicsList from "../comicsList/ComicsList";
+import AppBanner from "../appBanner/AppBanner";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 import decoration from '../../resources/img/vision.png';
 
 const App = () => {
-
+    
     const [selectedChar, setChar] = useState(null);
 
     const onCharSelected = (id) => {
@@ -19,7 +21,7 @@ const App = () => {
         <div className="app">
             <AppHeader/>
             <main>
-                <ErrorBoundary>
+                {/* <ErrorBoundary>
                     <RandomChar/>
                 </ErrorBoundary>
                 <div className="char__content">
@@ -30,7 +32,9 @@ const App = () => {
                         <CharInfo charId={selectedChar}/>
                     </ErrorBoundary>
                 </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
+                <img className="bg-decoration" src={decoration} alt="vision"/> */}
+                <AppBanner/>
+                <ComicsList/>
             </main>
         </div>
     )
